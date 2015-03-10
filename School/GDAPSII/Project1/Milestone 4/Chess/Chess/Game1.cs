ca5618128d174588503242e35d043e3de479b5e5
Margaret <margaretdorsey94@gmail.com>
2015-03-10 12:47:17 -0400

    first commit

---
+++ School/GDAPSII/Project1/Milestone 4/Chess/Chess/Game1.cs
@@ -0,0 +1,158 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace Chess
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        static GraphicsDeviceManager graphics;
+        static SpriteBatch spriteBatch;
+
+        #region Piece list attributes
+        public static List<Pawn> whitePawns; //all static so other classes can use them
+        public static List<Pawn> blackPawns;
+        public static List<Knight> whiteKnights;
+        public static List<Knight> blackKnights;
+        public static List<Bishop> whiteBishops;
+        public static List<Bishop> blackBishops;
+        public static List<Rook> whiteRooks;
+        public static List<Rook> blackRooks;
+        public static List<Queen> whiteQueen;
+        public static List<Queen> blackQueen;
+        public static King whiteKing;
+        public static King blackKing;
+        #endregion
+        static Texture2D boardBG;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            boardBG = Content.Load<Texture2D>("chessboard");
+
+            
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            #region Piece Initialization
+            //PAWNS
+            whitePawns = new List<Pawn>();
+            //add the white pawns from left to right
+            for (int i = 0; i < GameVariables.boardSpaceDim; i++)
+            {
+                //white is bottom, so y should be boardSpaceDim-1, white = true
+                whitePawns.Add(new Pawn(i, GameVariables.boardSpaceDim - 1, Content.Load<Texture2D>("whitePawn"), true));
+            }
+            blackPawns = new List<Pawn>();
+            //add the black pawns from left to right
+            for (int i = 0; i < GameVariables.boardSpaceDim; i++)
+            {
+                //black is bottom, so y should be 0, black = false
+                whitePawns.Add(new Pawn(i, 0, Content.Load<Texture2D>("blackPawn"), false));
+            }
+
+            //KNIGHTS
+            whiteKnights = new List<Knight>() { new Knight(1, GameVariables.boardSpaceDim - 1, Content.Load<Texture2D>("whiteKnight"), true), new Knight(6, GameVariables.boardSpaceDim - 1, Content.Load<Texture2D>("whiteKnight"), true) };
+            blackKnights = new List<Knight>() { new Knight(1, 0, Content.Load<Texture2D>("blackKnight"), false), new Knight(6, 0, Content.Load<Texture2D>("blackKnight"), false) };
+            //BISHOPS
+            whiteBishops = new List<Bishop>() { new Bishop(2, GameVariables.boardSpaceDim - 1, Content.Load<Texture2D>("whiteBishop"), true), new Bishop(5, GameVariables.boardSpaceDim - 1, Content.Load<Texture2D>("blackBishop"), true) };
+            blackBishops = new List<Bishop>() { new Bishop(2, 0, Content.Load<Texture2D>("blackBishop"), false), new Bishop(5, 0, Content.Load<Texture2D>("blackBishop"), false) };
+            //ROOKS
+            whiteRooks = new List<Rook>() { new Rook(0, GameVariables.boardSpaceDim - 1, Content.Load<Texture2D>("whiteRook"), true), new Rook(7, GameVariables.boardSpaceDim - 1, Content.Load<Texture2D>("whiteKnight"), true) };
+            blackRooks = new List<Rook>() { new Rook(0, 0, Content.Load<Texture2D>("blackRook"), false), new Rook(7, 0, Content.Load<Texture2D>("blackKnight"), false) };
+            //QUEENS
+            whiteQueen = new List<Queen>() { new Queen(3, GameVariables.boardSpaceDim - 1, Content.Load<Texture2D>("whiteQueen"), true) };
+            blackQueen = new List<Queen>(){new Queen(3, 0, Content.Load<Texture2D>("blackQueen"), false)};
+            //KINGS 
+            whiteKing = new King(4, GameVariables.boardSpaceDim - 1, Content.Load<Texture2D>("blackKing"), true);
+            blackKing = new King(4, 0, Content.Load<Texture2D>("blackKing"), false);
+            #endregion
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+           
+            
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(boardBG, new Rectangle(GameVariables.screenbufferHorizontal, GameVariables.screenbufferVertical, GameVariables.boardDim, GameVariables.boardDim), Color.White);
+
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+            
+        }
+
+        public static void AddPiece()
+        {
+            spriteBatch.Begin();
+            //add stuff here later, need some kind of selection thing to choose a new piece to add.
+            //remember to set the hasMoved attribute of any piece added to true
+            spriteBatch.End();
+        }
+    }
+}
---
+++ School/GDAPSII/Project1/Milestone 4/Chess/Chess/Pawn.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+
+namespace Chess
+{
+    //still needs en passant code
+    public class Pawn:Piece
+    {
+        
+        private Vector2 plannedMove = new Vector2();
+        public List<Vector2> attackSpace; //to account for the fact that pawns cannot attack straight on and therefore cannot have every space they can move to be a dangerous space.
+
+
+        public Pawn(int x, int y, Texture2D icon, bool color):base(x,y,icon,color)
+        {
+            attackSpace = new List<Vector2>(); 
+        }
+
+        public override void Select()
+        {
+            
+            possibleMoves = null; //clears out the list so it's a new set
+
+            //any forward pawn movement will need to be checked for collision - cannot capture unless diagonal
+            if(hasMoved == false) //first move a pawn makes
+            {
+                if (this.color == false) //gets to move two spaces on first move only
+                {
+                    plannedMove = new Vector2(location.X, location.Y + 2);
+                    if (this.PathTracker(this.location, new Vector2(plannedMove.X, plannedMove.Y + 1), "vertical")) //has to be messed with so we don't get a false positive - pawns can't capture vertically
+                        possibleMoves.Add(plannedMove);
+                }
+                if (this.color == true)
+                {
+                    plannedMove = new Vector2(location.X, location.Y - 2);
+                    if (this.PathTracker(this.location, new Vector2(plannedMove.X, plannedMove.Y - 1), "vertical"))
+                        possibleMoves.Add(plannedMove);
+                }
+                
+            }
+
+            //standard pawn movement
+            if (this.color == false)  //forward has different meanings for black and white pieces...
+            {
+                plannedMove = new Vector2(location.X, location.Y + 1);
+                if (this.PathTracker(this.location, new Vector2(plannedMove.X, plannedMove.Y+1), "vertical"))
+                    possibleMoves.Add(plannedMove);
+            }
+            if (this.color == true)
+            { 
+                plannedMove = new Vector2(location.X, location.Y - 1);
+                if (this.PathTracker(this.location, new Vector2(plannedMove.X,plannedMove.Y-1), "vertical"))
+                    possibleMoves.Add(plannedMove);
+            }
+            
+
+
+            //attacking requires no validation, only moving one space
+            //diagonally forward and to the right
+            if(this.color == false)
+                plannedMove = new Vector2(location.X+1, location.Y+1); //black pawn
+            if(this.color == true)
+                plannedMove = new Vector2(location.X+1, location.Y-1); //white pawn
+            if(GameVariables.board[(int)plannedMove.X,(int)plannedMove.Y] != null && GameVariables.board[(int)plannedMove.X,(int)plannedMove.Y].color != this.color) //piece in the space of an opposite color
+            {
+                possibleMoves.Add(plannedMove);
+                
+            }
+            attackSpace.Add(plannedMove); //even if the pawn can't move there now, it's an unsafe place for a king
+
+            //diagonally forward and to the left
+            if (this.color == false)
+                plannedMove = new Vector2(location.X-1, location.Y+1); //black pawn
+            if (this.color == true)
+                plannedMove = new Vector2(location.X-1, location.Y-1); //white pawn
+            if (GameVariables.board[(int)plannedMove.X, (int)plannedMove.Y] != null && GameVariables.board[(int)plannedMove.X, (int)plannedMove.Y].color != this.color) //piece in the space of an opposite color
+            {
+                possibleMoves.Add(plannedMove);
+            }
+            attackSpace.Add(plannedMove);
+
+            this.Trim(possibleMoves);
+            this.Trim(attackSpace);
+        }
+
+        public new void Move(Vector2 newLocation) //the new location should be well validated before making it here
+        {
+            //en passant hacky bullshit
+            if(!hasMoved&&Math.Abs(newLocation.Y - location.Y) == 2)
+            {
+                if(newLocation.Y < location.Y)
+                {
+                    GameVariables.board[(int)newLocation.X, (int)newLocation.Y - 1] = this;
+                }
+                else //covers greater than case
+                {
+                    GameVariables.board[(int)newLocation.X, (int)newLocation.Y + 1] = this;
+                }
+            }
+            else //cleanup from the hacky bullshit
+            {
+                if (GameVariables.board[(int)location.X, (int)location.Y + 1] == this)
+                    GameVariables.board[(int)location.X, (int)location.Y + 1] = null;
+
+                if (GameVariables.board[(int)location.X, (int)location.Y - 1] == this)
+                    GameVariables.board[(int)location.X, (int)location.Y - 1] = null;
+            }
+
+            GameVariables.board[(int)location.X, (int)location.Y] = null;
+            location = newLocation;
+            if (GameVariables.board[(int)location.X, (int)location.Y] != null && GameVariables.board[(int)location.X, (int)location.Y].color != this.color) //have to check to make sure there is a piece there before trying to look at its color
+            {
+                GameVariables.board[(int)location.X, (int)location.Y].Remove(this); //call the other piece's remove function
+            }
+            else
+            {
+                GameVariables.board[(int)location.X, (int)location.Y] = this; //put this piece there
+            }
+            
+            
+
+            if(color && location.Y == 0)
+            {
+                //get a new piece
+                this.Remove(this); //get rid of the pawn
+                Game1.AddPiece();
+ 
+                
+            }
+
+            if(!color && location.Y == GameVariables.boardSpaceDim -1)
+            {
+                //get a new piece 
+                this.Remove(this); //get rid of the pawn
+                Game1.AddPiece();
+            }
+
+            this.hasMoved = true; //completed the first move
+        }
+        
+    }
+}
