5a950d2f8ae04ed56ef0ea3b83eeb222198e4635
Margaret <margaretdorsey94@gmail.com>
2015-03-28 18:27:04 -0400

    added instructions to map tool

--- Desktop/Arbiter/MapTool/MapTool/MainForm.cs
+++ Desktop/Arbiter/MapTool/MapTool/MainForm.cs
@@ -13,15 +13,16 @@ namespace MapTool
 {
     public partial class MainForm : Form
     {
-        public InfoForm form;
-        public PictureBox[,] tiles;
-        public int[,] nums;
-        public int dim;
-        BinaryWriter writer;
-        TextBox textBox;
-        Button submitButton;
-        Label label;
-        Button randomButton;
+        public InfoForm form; //gets dimension from user
+        public PictureBox[,] tiles; //interface board
+        public int[,] nums; //back end board
+        public int dim; //dimension of board
+        BinaryWriter writer; //writes to save file
+        TextBox textBox; //for filename input
+        Button submitButton; //tries to save file
+        Label label; //for error output
+        Button randomButton; //Calls MapGenerator
+        Label instructionLabel; //gives instructions for the MapBuilder
         public MainForm()
         {
             InitializeComponent();
@@ -36,8 +37,7 @@ namespace MapTool
             dim = x;
             tiles = new PictureBox[x, x];
             nums = new int[x, x];
-            this.Width = 30 * x + 50;
-            this.Height = 30 * x + 200;
+            this.AutoSize = true; // form autosizes based on contents
             for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < x; j++)
@@ -66,23 +66,28 @@ namespace MapTool
                 }
             }
 
-            submitButton = new Button();
+            submitButton = new Button(); //for submitting custom map
             submitButton.Location = new System.Drawing.Point(30, 30 * x + 100);
             submitButton.Text = "Submit";
             submitButton.Click += new EventHandler(submitButton_Click);
             this.Controls.Add(submitButton);
 
-            randomButton = new Button();
+            randomButton = new Button(); //for randomizing the map
             randomButton.Location = new System.Drawing.Point(210, 30 * x + 100);
             randomButton.Text = "Randomize";
             randomButton.Click += new EventHandler(randomButton_Click);
             this.Controls.Add(randomButton);
 
 
-            textBox = new TextBox();
+            textBox = new TextBox(); //for file name input
             textBox.Location = new System.Drawing.Point(30, 30*x + 80);
             this.Controls.Add(textBox);
 
+            instructionLabel = new Label();
+            instructionLabel.Location = new System.Drawing.Point(5, 5);
+            instructionLabel.AutoSize = true; //allows label to stretch to fit text
+            instructionLabel.Text = "Click on a tile to change it. Enter a file name and click Submit when finished.";
+            this.Controls.Add(instructionLabel);
         }
 
         private void pictureBox_Click(object sender, System.EventArgs e)
