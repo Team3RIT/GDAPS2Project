b67b29b60c168566ba728aa4ac3d44aa67ddbcd1
Margaret <margaretdorsey94@gmail.com>
2015-04-02 15:58:39 -0400

    try try again

--- Desktop/Arbiter/Arbiter/Player.cs
+++ Desktop/Arbiter/Arbiter/Player.cs
@@ -11,8 +11,11 @@ namespace Arbiter
         #region attributes
         string name; //player name
         int playerNum; //should be 1 to 4, the "player" that owns the immobile pieces will be player 0.
-        int movedUnits; //number of units moved by the player during their turn. --Sorry for touching your code! -Travis
-
+        public StandardUnit pi1;
+        StandardUnit pi2;
+        StandardUnit pi3;
+        StandardUnit pi4;
+        StandardUnit pi5;
         #endregion
 
         #region constructor
@@ -20,7 +23,16 @@ namespace Arbiter
         {
             name = nm;
             playerNum = playerNumber;
-            movedUnits = 0;
+        }
+        public Player(string nm, int playerNumber, int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4, int x5, int y5)
+        {
+            name = nm;
+            playerNum = playerNumber;
+            pi1 = new StandardUnit(x1,y1,this);
+            pi2 = new StandardUnit(x2,y2,this);
+            pi3 = new StandardUnit(x2,y2,this);
+            pi4 = new StandardUnit(x2,y2,this);
+            pi5 = new StandardUnit(x2,y2,this);
         }
         #endregion
 
@@ -36,17 +48,6 @@ namespace Arbiter
             get
             {
                 return name;
-            }
-        }
-        public int MovedUnits
-        {
-            get
-            {
-                return movedUnits;
-            }
-            set
-            {
-                movedUnits = value;
             }
         }
     }
